a84f4fc2b9cc5f48dc419bec76022b555e5a320d
Ivan <vano148@bk.ru>
2021-12-10 11:05:31 +0300

    Create new module for bullet -  bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,12 @@ class AlienInvasion:
         '''Инициализирует игру и создает игровые ресурсы'''
         pygame.init()
         self.settings = Settings()
+        
+        #Запуск в полноэкранном режиме
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
@@ -38,6 +44,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
--- settings.py
+++ settings.py
@@ -8,4 +8,12 @@ class Settings():
         self.screen_height = 400
         self.bg_color = (0, 0, 0)
 
+        # Скорость передвежения корабля
         self.ship_speed_factor = 0.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
